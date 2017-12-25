cc8710577872cceaecd48b30ea98781b8e0a6aae
mantzaris <avmantzaris@gmail.com>
2017-12-24 23:57:16 -0500

    first step here

---
+++ countryBiasESC.jl
@@ -0,0 +1,9 @@
+#GOALS
+#1 examine biases over a set of years because many things change
+#2 compare the biases and rank them
+#3 look for consistency in the bias distribution
+#4 plot the bias ranking for countries as they appear in time series
+#5 plot percentage of accumulated score of bias
+#6 look at the high ranking as well as the low ranking
+#7 table for the symmetric ignoring
+
