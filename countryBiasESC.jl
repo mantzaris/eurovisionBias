d0a10d5cc80b3f055aa7c458c6e2a544d9bf348e
mantzaris <avmantzaris@gmail.com>
2017-12-25 23:37:21 -0500

    adding basic number checking

--- countryBiasESC.jl
+++ countryBiasESC.jl
@@ -12,41 +12,56 @@
 #MAIN<<
 function biasesESC(startYear = 1980, endYr = 1990)
 
-    #load data
-    dataParse()
+    #load data and get the dictionary for the country num per year
+    countryYearsNum, yrMin, yrMax = dataCountryYearsNum()
     
     #check params
-    paramCheck(startYear, endYr)
-    
+    paramCheck(startYear, endYr, yrMin, yrMax)
+    println("$startYear, $endYr, $yrMin, $yrMax")
+    println(countryYearsNum)
 end
 
 
-#fn to iterate over the ESC datafiles
-function dataParse()
+#get the years of the data provided and country number in each year
+function dataCountryYearsNum()
+    countryYearsNum = Dict{Integer,Integer}()
     resultsFile = readdir("./dataTables/")
     yrMin = 100000
     yrMax = -1
     for rf in resultsFile
-        
-        
+        fileTmp = open(string("./dataTables/",rf))
+        linesTmp = readlines(fileTmp)  #readfile lines
+        yrTmp = parse(Int,((split(rf,"."))[1]))
+        countryNumTmp = length(split(linesTmp[1],",")) - 1
+        countryYearsNum[yrTmp] = countryNumTmp 
+        close(fileTmp)
+        if(yrTmp < yrMin)
+            yrMin = yrTmp
+        end
+        if(yrTmp > yrMax)
+            yrMax = yrTmp
+        end
     end
-
-
+        
+    return countryYearsNum, yrMin, yrMax
 end
 
 
 
-
-
 #fn to accept the parameters and check for the validity
-function paramCheck(startYear, endYr)
-    if(startYear >= endYr)
+function paramCheck(startYr, endYr, yrMin, yrMax)
+    if(startYr >= endYr)
         println("the start year needs to be before the end year")
-        return -1
+        quit()
     end
-    if((startYear+4) >= endYr)
+    if((startYr+4) >= endYr)
         println("the start year needs to be at least 4 years prior the end year")
+        quit()
     end
-    
-    
+    #sanity check input years
+    if( endYr < startYr || startYr < yrMin || endYr > yrMax )
+        print(string("year range improperly set, for the analysis end year must be greater than start and the smallest year is $(yrMin) and largest $(yrMax) with smallest first"))
+        quit()
+    end
+      
 end
