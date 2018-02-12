f311f7e7e453598ed68c4acdc395a37f88ea9405
mantzaris <avmantzaris@gmail.com>
2018-02-11 22:34:15 -0500

    additions to the creation of the adjacency lists in matrix form

--- csvDir2AdjList.jl
+++ csvDir2AdjList.jl
@@ -1,5 +1,41 @@
 #Doing things another way, even if not better, not over thinking this, which is not what a programming mindset is supposed to do, but jumping in.
 
+#look at the directory and load every year in the range of the .csv files, then produce a final matrix which is the countryFrom|countryTo|year|score for this 'subset'
+function yearsScoreAdjList(startYr = 1980, endYr = 1990)
+    dirFiles = readdir("./dataTables/")
+    adjMatScore = []
+    for dF in dirFiles
+        yrTmp = parse(Int,((split(dF,"."))[1]))
+        if(yrTmp < startYr || yrTmp > endYr)
+            continue
+        end
+        fileTmp = open(string("./dataTables/",dF))
+        linesTmp = readlines(fileTmp)#read each file lines
+        origColNames = split(linesTmp[1],r",|\n",keep=false)#get line1 into components      
+        for ii=2:length(linesTmp)#get the names on the rows
+            rowTmp = split(linesTmp[ii],r",|\n",keep=false)
+            for jj=2:length(origColNames)
+                if(ii!=jj)
+                    #print("$(rowTmp[1])->$(origColNames[jj])=$(rowTmp[jj]),")
+                    if(isempty(adjMatScore) == true)
+                        adjMatScore = Array{Any}(1,4)
+                        adjMatScore[1,:] = [rowTmp[1],origColNames[jj],yrTmp,parse(Int,rowTmp[jj])]
+                    else
+                        #println([rowTmp[1],origColNames[jj],yrTmp,parse(Int,rowTmp[jj])])
+                        adjMatScore = vcat(adjMatScore , [rowTmp[1] origColNames[jj] yrTmp parse(Int,rowTmp[jj])])
+                    end                    
+                end                
+            end            
+        end
+       
+    end
+    return adjMatScore   
+end
+
+
+
+
+
 
 #look at the directory and load every .csv file, then produce a final matrix which is the countryFrom|countryTo|year|score
 function totalScoreAdjList()   
