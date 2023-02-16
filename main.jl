41d0fca1b38d0c250aefaec7ed81f03f6ff7ec06
mantzaris <avmantzaris@gmail.com>
2023-02-16 11:53:29 -0500

    now there is a results folder holding all the results

--- main.jl
+++ main.jl
@@ -43,4 +43,35 @@ function main(startYr = 1980, endYr = 1990, windowSize = 5, alpha = 0.05)
     
     graphAvoid(biasESC_uppper_lower[1],biasESC_uppper_lower[2])
 
+
+
+    ############################
+    try
+        mkdir("./results")
+    catch
+        println("dirs exist")
+    end
+    try
+        mkdir("./results/graphs")
+    catch
+        println("dirs exist")
+    end
+    try
+        mkdir("./results/plots")
+    catch
+        println("dirs exist")
+    end
+
+    try
+        cp("./plots/","./results/plots",force=true)
+    catch
+        println("can't mv to results")
+    end
+    try
+        cp("./graphs","./results/graphs",force=true)
+    catch
+        println("can't mv to results")
+    end
+    
+    
 end
